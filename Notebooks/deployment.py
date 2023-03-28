bb0d98ccaf50d05940abba46993059b16d1ee8a6
Alex Kobetich <akobetich@gmail.com>
2023-03-28 19:55:44 -0400

    models saved

---
+++ Notebooks/deployment.py
@@ -0,0 +1,11 @@
+# import Flask and jsonify
+from flask import Flask, jsonify, request
+# import Resource, Api and reqparser
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import numpy
+import pickle
+
+app = Flask(__name__)
+api = Api(app)
+
